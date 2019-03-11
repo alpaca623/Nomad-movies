7eb0b66e3571082b2d1e4b6460fec38157a60c67
alpaca <alpaca@alpacaui-MacBookPro.local>
2019-03-11 17:39:06 +0900

    2.1 Custom Icons for the Tab Navigation

--- Navigation/TabNavigation.js
+++ Navigation/TabNavigation.js
@@ -1,17 +1,54 @@
+import React from "react";
 import { createBottomTabNavigator, createAppContainer } from "react-navigation";
-import TVScreen from "../screens/TV";
-import MovieScreen from "../screens/Movies";
-import SearchScreen from "../screens/Search";
+import { Platform } from "react-native";
+import TVScreen from "../Screens/TV";
+import MovieScreen from "../Screens/Movies";
+import SearchScreen from "../Screens/Search";
 import { BG_COLOR } from "../Constants/Colors";
 
+import TabBarIcon from "../Components/TabBarIcon";
+
+// 첫번째 매개변수는 RouteConfigs이고,
+// 두번째 매개변수는 BottomTabNavigatorConfig이다.
 const TabNavigation = createBottomTabNavigator(
   {
-    MovieScreen,
-    TVScreen,
-    SearchScreen
+    Movie: {
+      screen: MovieScreen,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon
+            focused={focused}
+            name={Platform.OS === "ios" ? "ios-film" : "md-film"}
+          />
+        )
+      }
+    },
+    TV: {
+      screen: TVScreen,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon
+            focused={focused}
+            name={Platform.OS === "ios" ? "ios-tv" : "md-tv"}
+          />
+        )
+      }
+    },
+    Search: {
+      screen: SearchScreen,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon
+            focused={focused}
+            name={Platform.OS === "ios" ? "ios-search" : "md-search"}
+          />
+        )
+      }
+    }
   },
   {
     tabBarOptions: {
+      showLabel: false,
       style: {
         backgroundColor: BG_COLOR
       }
