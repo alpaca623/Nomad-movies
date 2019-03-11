1dead3fb9a2153df8372f90f5f7527d6cd22aa4d
alpaca <alpaca@alpacaui-MacBookPro.local>
2019-03-11 16:01:55 +0900

    1.2 Pre Loading Assets

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { AppLoading, Font } from "expo";
+import { IonIcons } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from "react-native";
 
 export default class App extends React.Component {
+  state = {
+    loaded: false
+  };
+
+  handleError = error => console.log(error);
+
+  // 로딩이 끝난 다음에 발생하는 함수
+  handleLoaded = () => this.setState({ loaded: true });
+
+  loadAssets = async () => {
+    await Font.loadAsync({
+      ...IonIcons.font
+    });
+  };
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    const { loaded } = this.state;
+    if (loaded) {
+      return (
+        <View style={styles.container}>
+          <Text>Hello!</Text>
+        </View>
+      );
+    } else {
+      return (
+        <AppLoading
+          startAsync={this.loadAssets}
+          onFinish={this.handleLoaded}
+          onError={this.handleError}
+        />
+      );
+    }
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
